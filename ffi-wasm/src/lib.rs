301894c51a2d121dcea43af4d0b86065fea1786d
jalho <alho.jere@gmail.com>
2025-07-29 20:48:56 +0300

    Add class

--- ffi-wasm/src/lib.rs
+++ ffi-wasm/src/lib.rs
@@ -2,3 +2,22 @@
 pub fn get_one() -> u32 {
     return 1;
 }
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub fn make_log_entry() -> LogEntry {
+    LogEntry {
+        instant: chrono::DateTime::from_timestamp_nanos(0),
+    }
+}
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+pub struct LogEntry {
+    instant: chrono::DateTime<chrono::Utc>,
+}
+
+#[wasm_bindgen::prelude::wasm_bindgen]
+impl LogEntry {
+    pub fn as_timestamp(&self) -> i64 {
+        return self.instant.timestamp_millis();
+    }
+}
--- main.mts
+++ main.mts
@@ -1,7 +1,13 @@
 import * as lib from "./ffi-wasm/pkg/ffi_wasm.js";
 
 async function main() {
-  const value = lib.get_one();
-  console.log("DEBUG", value);
+  const num_1: number = lib.get_one();
+  console.log("DEBUG num_1", num_1);
+
+  const log_entry: lib.LogEntry = lib.make_log_entry();
+  const timestamp: bigint = log_entry.as_timestamp();
+  console.log("DEBUG timestamp", timestamp);
+
+  log_entry.free();
 }
 main();
